c34e6413ff7e420cbef8376f3c86d54c88ed3b3a
Yandr301187 <sharkievich@gmail.com>
2022-07-20 15:17:16 +0300

    Фdded a method for finding the maximum value in an array and tests for this method

--- src/itStep/yandr/javaStages/stage13/generalTask/model/logic/ArrayManager.java
+++ src/itStep/yandr/javaStages/stage13/generalTask/model/logic/ArrayManager.java
@@ -1,4 +1,23 @@
 package itStep.yandr.javaStages.stage13.generalTask.model.logic;
 
+import itStep.yandr.javaStages.stage13.generalTask.exception.InvalidSizeOfArray;
+
 public class ArrayManager {
+
+    private static void ValidateIntegerArray(int[] array) throws InvalidSizeOfArray {
+        if (array == null || array.length < 2) {
+            throw new InvalidSizeOfArray();
+        }
+    }
+
+    public static int findMaxValueIndex(int[] array) throws InvalidSizeOfArray {
+        ValidateIntegerArray(array);
+        int max = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
 }
