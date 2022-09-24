34b9184226bd439d28b257980c1ef67b72fe990e
Yandr2022 <sharkievich@gmail.com>
2022-09-24 12:35:32 +0300

    CountMultipleElements

--- src/itStep/yandr/javaStages/stage14/levelB/model/logic/CounterElements.java
+++ src/itStep/yandr/javaStages/stage14/levelB/model/logic/CounterElements.java
@@ -4,18 +4,20 @@ import itStep.yandr.javaStages.stage14.levelB.model.logic.strategy.CountMultiple
 import itStep.yandr.javaStages.stage14.levelB.model.logic.strategy.ElementCountable;
 
 public class CounterElements {
-   public static final ElementCountable MULTIPLE = new CountMultipleElements();
+    public static final ElementCountable MULTIPLE = new CountMultipleElements();
 
-    public static int count(ElementCountable countable,int number, int... values) {
-        if ((values == null || values.length == 0)||(countable.equals (MULTIPLE) && number==0)) {
+    public static int count(ElementCountable countable, int number, int... values) {
+        if ((values == null || values.length == 0) || (countable == null) || (countable.equals(MULTIPLE) && number == 0)) {
             return -1;
         }
         int count = 0;
         for (int value : values) {
-            if (countable.relevant(number, value)) {
-                count ++;
+//            if (!(countable.equals (MULTIPLE) && value==0)){
+            if (countable.relevant(value, number)) {
+                count++;
             }
         }
+
         return count;
     }
 }
